a18586d971e2ae3517f3be5985fabe16ee5bbc56
Alex <alexsolo007@hotmail.com>
2014-10-27 09:19:10 +0100

    clase person creada

---
+++ Person.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+}
